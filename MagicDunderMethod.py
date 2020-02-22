bd5235e84fb09b146a831a4985ece25e8c4a968e
Md Imtiaz Chowdhury <39957822+mdimtiaz501@users.noreply.github.com>
2020-02-22 14:50:52 +0600

    Update MagicDunderMethod.py

--- MagicDunderMethod.py
+++ MagicDunderMethod.py
@@ -12,7 +12,7 @@ class MDM2:
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        print("Hello prithibi")
+        print("Hello prithibi",self.y)
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
